d093aae98e01a8c2c66ba2150971d48b6c46bd46
Marcos Oliveira <marcos@terminalroot.com.br>
2023-09-06 17:16:43 -0300

    -o cpf build.lua

--- build.lua
+++ build.lua
@@ -2,4 +2,4 @@ if(#arg > 0 and arg[1] == ("--check" or "-c")) then
   os.execute("cppcheck --suppress=missingIncludeSystem *.cpp")
 end
 
-os.execute("/usr/bin/g++ -Wall -Werror -Wextra -pedantic -g -fsanitize=address -O2 *.cpp")
+os.execute("/usr/bin/g++ -Wall -Werror -Wextra -pedantic -g -fsanitize=address -O2 -o cpf *.cpp")
